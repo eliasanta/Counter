cf1b376e050716def3ef4e952dcc1766a3e0a7c1
eliasanta <eliasanta93@gmail.com>
2022-04-01 12:29:07 +0200

    final upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,50 @@
-//get input with differente way 
-const counter = document.getElementsByClassName('counter')[0];
-const colourBtns = document.querySelectorAll('div.colour-btns>button');
-const element = document.querySelector('.counter-obj');
-const write=document.querySelector('.back');
+//get input
+const counter = document.getElementsByClassName("counter")[0];
+const colourBtns = document.querySelectorAll("div.colour-btns>button");
+const element = document.querySelector(".counter-obj");
+const write = document.querySelector(".back");
 //functions
 const increase = () => counter.innerText++;
 const decrease = () => counter.innerText--;
-const setColour = (event) => (counter.style.color = event.target.style.backgroundColor);
+const setColour = (event) =>
+  (counter.style.color = event.target.style.backgroundColor);
 
 function resetCounter() {
-    counter.innerHTML = 0;
-    
+  counter.innerHTML = 0;
 }
 
-colourBtns.forEach(btn => btn.addEventListener('click', setColour));
+colourBtns.forEach((btn) => btn.addEventListener("click", setColour));
 
-function removeCounter(){
-element.classList.add('animate__animated', 'animate__bounceOutLeft');
-write.style.visibility = "visible";
-
-
+function removeCounter() {
+  element.classList.add("animate__animated", "animate__bounceOutLeft");
+  setTimeout(createImage, 900);
+  sleeper();
 }
-function blink_one() {
-    document.getElementById('blink').style.visibility='visible';
-    setTimeout('blink_two()',700);
-    }
-function blink_two() {
-    document.getElementById('blink').style.visibility='hidden';
-    setTimeout('blink_one()',700);
-    }
- blink_one();
+
+function darkMode() {
+  document.getElementsByTagName("body")[0].style.backgroundColor="rgb(107, 116, 130)";
+}
+function whiteMode() {
+  document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+}
+
+function createImage() {
+  var createText = document.getElementById("text");
+  var text = document.createTextNode("Good Night!, Sleep Weel...");
+  createText.appendChild(text);
+  var img = document.createElement("img");
+  img.src ="https://i.pinimg.com/564x/06/92/83/0692834ef41e618762c784066d0a5f24.jpg";
+  var block = document.getElementById("x");
+
+  block.appendChild(img).style.marginTop = "90px";
+  block.appendChild(img).style.marginBottom = "50px";
+  block.appendChild(img).style.width = "250px";
+  block.appendChild(img).style.borderRadius='10px';
+
+  document.getElementById("sheep").style.position = "absolute";
+  document.getElementById("sheep").style.left = "50%";
+  document.getElementById("sheep").style.transform = "translate(-50%)";
+}
+function sleeper() {
+  document.getElementById("sleep").style.visibility = "hidden";
+}
